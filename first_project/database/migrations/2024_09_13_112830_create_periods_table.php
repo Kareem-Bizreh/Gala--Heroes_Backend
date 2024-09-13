16795197ea7a4ccc6fc2497b50c0e45ab3674ec9
Nour alden Khlil <nouralden.khlil.04@gmail.com>
2024-09-13 17:23:31 +0300

    create periods table + create categories table + create products table

---
+++ first_project/database/migrations/2024_09_13_112830_create_periods_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('periods', function (Blueprint $table) {
+            $table->id();
+            $table->json('periods_arr');
+            $table->json('rates_arr');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('periods');
+    }
+};
---
+++ first_project/database/migrations/2024_09_13_135240_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id')->constrained('users')->references('id');
+            $table->string('name');
+            $table->string('image_url');
+            $table->foreignId('category_id')->constrained('categories')->references('id');
+            $table->float('price');
+            $table->date('expiration_date');
+            $table->foreignId('period_id')->constrained('periods')->references('id');
+            $table->integer('count');
+            $table->integer('views');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
